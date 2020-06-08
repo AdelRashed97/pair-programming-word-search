809c6537f010726250a417559f786a8081b10814
Adel Rashed <adel_rashed@outlook.com>
2020-06-08 00:41:17 +0000

    completed function and added more test cases

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,22 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['C', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['A', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['C', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['T', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['E', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['R', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHARACTER')
+
+    assert.isTrue(result);
+  });
+
+
 });
